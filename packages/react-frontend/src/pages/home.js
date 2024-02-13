6e4f05293e105824250e9cf95948ce05e9d06723
ndullagh <113929865+ndullagh@users.noreply.github.com>
2024-02-13 15:37:17 -0800

    Should work now

---
+++ packages/react-frontend/src/pages/home.js
@@ -0,0 +1,28 @@
+// pages/about.js
+
+import React from "react";
+
+const Home = () => {
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "centre",
+				alignItems: "centre",
+				height: "100vh",
+                background: "#dce3e6",
+                textAlign: "center"
+                
+			}}
+		>
+			<center>
+                <h1>
+				    Home page
+			    </h1>
+            </center>
+
+		</div>
+	);
+};
+
+export default Home;
---
+++ packages/react-frontend/src/pages/sign-in.js
@@ -0,0 +1,28 @@
+// pages/about.js
+
+import React from "react";
+
+const SignIn = () => {
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "centre",
+				alignItems: "centre",
+				height: "100vh",
+                background: "#dce3e6",
+                textAlign: "center"
+                
+			}}
+		>
+			<center>
+                <h1>
+				    Sign-in page
+			    </h1>
+            </center>
+
+		</div>
+	);
+};
+
+export default SignIn;
