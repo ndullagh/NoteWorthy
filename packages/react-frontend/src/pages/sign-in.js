d9139233eed0f58311a1e8395eb9762a468fc4fc
sgill33 <sgill33@calpoly.edu>
2024-02-17 19:12:15 -0800

    removed infile styling

--- packages/react-frontend/src/pages/sign-in.js
+++ packages/react-frontend/src/pages/sign-in.js
@@ -2,23 +2,16 @@
 
 import React from "react";
 
-const SignIn = () => {
+export default function SignIn () {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "centre",
-        alignItems: "centre",
-        height: "100vh",
-        background: "#dce3e6",
-        textAlign: "center"
-      }}
-    >
+    <>
       <center>
         <h1>Sign-in page</h1>
       </center>
-    </div>
-  );
-};
+    </>
 
-export default SignIn;
+
+  );
+}
+
+
