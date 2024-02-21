fb40181f982235af92d6c58ccd2c15550d094f2d
sgill33 <sgill33@calpoly.edu>
2024-02-20 19:47:41 -0800

    added searchbar and notebook creation button

--- packages/react-frontend/src/pages/notebooks.js
+++ packages/react-frontend/src/pages/notebooks.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Notebook from "../components/Notebook/notebook";
+import { SearchBar } from "../components/Searchbar/searchbar";
+import { Stack, Button } from "@chakra-ui/react";
+import "../styles/notepage.css"
 
 export default function Notebooks () {
     return (
       <div className="notePageBody">
-        <h1>Notebook Page</h1>
+        <h1>Your Notebooks</h1>
         <Notebook title={"Here is a long title"} slug={""} color = {"lightskyblue"}></Notebook>
         <Notebook title={"Short title"} slug={""} color = {"purple"}></Notebook>
+
+        <Stack padding={5}spacing={4} direction="row" align="center" pos="fixed" bottom={16} width={"100%"} >
+          <SearchBar />
+          <Button variant="solid" color={"white"} backgroundColor={"#0ba6ff"}>New Notebook</Button>
+        </Stack>
+
+
+
       </div>
     );
   }
