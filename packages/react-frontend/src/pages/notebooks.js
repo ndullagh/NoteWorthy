e6750a0daa16532c19e0b4fbe70ba9b3ea9f7d02
sgill33 <sgill33@calpoly.edu>
2024-02-21 15:26:53 -0800

    made notebooks link to pages page

--- packages/react-frontend/src/pages/notebooks.js
+++ packages/react-frontend/src/pages/notebooks.js
@@ -11,8 +11,8 @@ export default function Notebooks () {
       <div className="notePageBody">
         
         <h1>Your Notebooks</h1>
-        <Notebook title={"Here is a long title"} slug={""} color = {"lightskyblue"}></Notebook>
-        <Notebook title={"Short title"} slug={""} color = {"purple"}></Notebook>
+        <Notebook title={"Here is a long title"} slug={"notebook/pages"} color = {"lightskyblue"}></Notebook>
+        <Notebook title={"Short title"} slug={"notebook/pages"} color = {"purple"}></Notebook>
 
         <Stack padding={5}spacing={4} direction="row" align="center" pos="fixed" bottom={16} width={"100%"} >
           <SearchBar />
