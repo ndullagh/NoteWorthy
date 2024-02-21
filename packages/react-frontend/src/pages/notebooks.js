1dea03e0bc985f339650546c94dff7a331cdc4a7
sgill33 <sgill33@calpoly.edu>
2024-02-20 20:48:17 -0800

    added modal button to bottom of page

--- packages/react-frontend/src/pages/notebooks.js
+++ packages/react-frontend/src/pages/notebooks.js
@@ -1,19 +1,22 @@
 import React from "react";
 import Notebook from "../components/notebook";
 import { SearchBar } from "../components/searchbar";
-import { Stack, Button } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
+import {NoteModal} from "../components/NoteModal"
+
 
 
 export default function Notebooks () {
     return (
       <div className="notePageBody">
+        
         <h1>Your Notebooks</h1>
         <Notebook title={"Here is a long title"} slug={""} color = {"lightskyblue"}></Notebook>
         <Notebook title={"Short title"} slug={""} color = {"purple"}></Notebook>
 
         <Stack padding={5}spacing={4} direction="row" align="center" pos="fixed" bottom={16} width={"100%"} >
           <SearchBar />
-          <Button variant="solid" color={"white"} backgroundColor={"#0ba6ff"}>New Notebook</Button>
+          <NoteModal/>
         </Stack>
       </div>
     );
