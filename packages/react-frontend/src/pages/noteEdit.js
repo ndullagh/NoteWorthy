69784f59a504a683453bab9d605b07fe7b8db51c
sgill33 <sgill33@calpoly.edu>
2024-02-20 21:04:26 -0800

    created pages for rest of my workflow

---
+++ packages/react-frontend/src/pages/noteEdit.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NoteEdit () {
+    return (
+      <div>
+        <center>
+          <h1>Create Note.</h1>
+        </center>
+      </div>
+    );
+  }
---
+++ packages/react-frontend/src/pages/pages.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Pages () {
+    return (
+      <div>
+        <center>
+          <h1>Pages Page</h1>
+        </center>
+      </div>
+    );
+  }
