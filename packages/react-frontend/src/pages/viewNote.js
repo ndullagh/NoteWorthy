bf4c0953217398de73b881475c45998d4e5ccaa5
sgill33 <sgill33@calpoly.edu>
2024-02-24 21:01:59 -0800

    page now loads and displays formatted note

--- packages/react-frontend/src/pages/viewNote.js
+++ packages/react-frontend/src/pages/viewNote.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Box } from "@chakra-ui/react";
 
 export default function ViewNote() {
+  const noteval = { _id: "65dac6256dcd1b5c6f26531b" };
+
+  const [note, setNote] = useState([]);
+
+  function fetchNote(note_id) {
+    const promise = fetch(
+      `http://localhost:8000/notes?_id=${note_id}`
+    );
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchNote(noteval._id)
+      .then((res) => res.json())
+      .then((json) => setNote(json))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>
-      <center>
-        <h1>View Note</h1>
-      </center>
+      <Box pl={5}>
+        <div
+          dangerouslySetInnerHTML={{ __html: note.contents }}
+        />
+      </Box>
     </div>
   );
 }
