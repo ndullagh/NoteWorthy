16e33ce3e08cb95ecaec6d3b6b7da97a712597c2
sgill33 <sgill33@calpoly.edu>
2024-03-06 19:52:50 -0800

    added and connected delete button

--- packages/react-frontend/src/pages/home.js
+++ packages/react-frontend/src/pages/home.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "../styles/homepage.css";
 import { jwtDecode } from "jwt-decode";
 import Cookies from "js-cookie";
+import { addAuthHeader } from "../auth";
 
 const Home = () => {
   const [decoded, setDecoded] = useState(null);
@@ -10,15 +11,62 @@ const Home = () => {
   if (Cookies.get("token") && !decoded) {
     setDecoded(jwtDecode(Cookies.get("token")));
   }
+
+  function fetchUser(username) {
+    const promise = fetch(
+      `http://localhost:8000/users?username=${username}`,
+      {
+        method: "GET",
+        headers: addAuthHeader(
+          {
+            "Content-Type": "application/json"
+          },
+          Cookies.get("token")
+        )
+      }
+    );
+    return promise;
+  }
+
+  function deleteUser(user_id) {
+    const promise = fetch(
+      `Http://localhost:8000/users/${user_id}`,
+      {
+        method: "DELETE",
+        headers: addAuthHeader(
+          {
+            "Content-Type": "application/json"
+          },
+          Cookies.get("token")
+        )
+      }
+    );
+
+    return promise;
+  }
+
+  function handleDelete(){
+    fetchUser(decoded.username) //fetch user from username
+      .then((res) => res.json())
+      .then((json) => {
+        deleteUser(json[0]._id) //fetch notebooks from user using user_id
+          .then(Cookies.remove("token"))
+          .then(location.reload())
+
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   return (
     <div className="homePage">
       <main>
         <div className="homeBody">
           {decoded ?  
-            <h1 className="homeText">
-            Welcome <br></br> {decoded.username}
+            <h1 className="greeting">
+            Welcome, {decoded.username}
             </h1>
-          
+
           :           
             <h1 className="homeText">
               Welcome To <br></br> Noteworthy
@@ -34,6 +82,7 @@ const Home = () => {
           </Link>
           {
             Cookies.get("token") ?
+            <>
             <Link
               className="button"
               style={{ color: "white" }}
@@ -46,6 +95,19 @@ const Home = () => {
               >
               Logout.
             </Link>
+            <Link
+              className="button"
+              style={{ color: "white" }}
+              onClick={() => 
+                {
+                  handleDelete()
+
+                }}
+              >
+              Delete Account.
+            </Link>
+
+            </>
           :
             <></>
           }
