f4a2a1cdd9c177a5ace9c82390062fad29d05f0a
robintun <71895570+robintun@users.noreply.github.com>
2024-03-12 18:04:45 -0700

    deleted commented code

--- packages/react-frontend/src/auth.js
+++ packages/react-frontend/src/auth.js
@@ -1,33 +1,6 @@
 import Cookies from "js-cookie";
 import { AZURE_DOMAIN } from "./config";
 
-// export function loginUser(creds) {
-//   const promise = fetch(`${AZURE_DOMAIN}/login`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify(creds)
-//   })
-//     .then((response) => {
-//       if (response.status === 200) {
-//         response.json().then((payload) => {
-//           Cookies.set("token", payload.token, {
-//             expires: 1,
-//             secure: true
-//           });
-//         });
-//       } else {
-//         `Login Error ${response.status}: ${response.data}`;
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       console.log("Error status code:", error.statusCode);
-//     });
-
-//   return promise;
-// }
 
 export function loginUser(creds) {
   return fetch(`${AZURE_DOMAIN}/login`, {
@@ -58,34 +31,6 @@ export function loginUser(creds) {
       throw error;
     });
 }
-
-// export function signupUser(creds) {
-//   const promise = fetch(`${AZURE_DOMAIN}/signup`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify(creds)
-//   })
-//     .then((response) => {
-//       if (response.status === 201) {
-//         response.json().then((payload) => {
-//           Cookies.set("token", payload.token, { secure: true });
-//         });
-//       } else {
-//         console.log(
-//           `Signup Error ${response.status}: ${response.data}`
-//         );
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       console.log("Error status code:", error.statusCode);
-//       throw error;
-//     });
-
-//   return promise;
-// }
 
 export function signupUser(creds) {
   const promise = fetch(`${AZURE_DOMAIN}/signup`, {
